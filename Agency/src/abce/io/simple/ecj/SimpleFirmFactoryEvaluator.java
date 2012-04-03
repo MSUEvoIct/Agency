2378c22dfbf95c7b46c58e4416a1b8bb081a6139
Matthew Rupp <ruppmatt@gmail.com>
2012-04-03 14:34:59 -0400

    Corrected parameter database path to stimulus response types.

--- Agency/src/abce/io/simple/ecj/SimpleFirmFactoryEvaluator.java
+++ Agency/src/abce/io/simple/ecj/SimpleFirmFactoryEvaluator.java
@@ -121,12 +121,13 @@ public class SimpleFirmFactoryEvaluator extends Evaluator {
 	 */
 	protected static Class<? extends StimulusResponse>[] getSRClasses(EvolutionState state, int sp_ndx)
 			throws ClassNotFoundException {
-		Parameter species_base = new Parameter("pop").push("subpop").push(Integer.toString(sp_ndx)).push("species");
+		Parameter species_base = new Parameter("pop").push("subpop").push(Integer.toString(sp_ndx)).push("species")
+				.push("ind");
 		int num_trees = state.parameters.getInt(species_base.push("numtrees"), null);
 		@SuppressWarnings("unchecked")
 		Class<? extends StimulusResponse>[] sr_classes = (Class<? extends StimulusResponse>[]) new Class<?>[num_trees];
 		for (int tree_ndx = 0; tree_ndx < num_trees; tree_ndx++) {
-			Parameter tree_base = species_base.push(Integer.toString(sp_ndx));
+			Parameter tree_base = species_base.push("tree").push(Integer.toString(sp_ndx));
 			String sr_name = state.parameters.getString(tree_base.push("sr"), null);
 			@SuppressWarnings("unchecked")
 			Class<? extends StimulusResponse> sr_class = (Class<? extends StimulusResponse>) Class.forName(sr_name);
