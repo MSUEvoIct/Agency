5aaf1aa351729869baed3392973daee95af62b69
kkoning <kkoning@destiny.local>
2012-05-02 14:19:23 -0400

    Added documentation and tracing information for Simulation Actions. Refactored function names to be more intuitive/descriptive.

---
+++ Agency/src/abce/agency/engine/TraceConfig.java
@@ -0,0 +1,43 @@
+package abce.agency.engine;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import abce.agency.actions.SimulationAction;
+
+public class TraceConfig implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private static TraceConfig singleton;
+	
+	public static TraceConfig getTraceConfig() {
+		if (singleton == null)
+			singleton = new TraceConfig();
+		return singleton;
+	}
+
+	public final boolean traceEnabled;
+
+	private Map<? extends Class<? extends SimulationAction>, Boolean> actionTraces;
+	
+	public TraceConfig() {
+		traceEnabled = true;
+	}
+	
+	
+	/**
+	 * @param actionClass
+	 * @return true if the specified action should emit trace information, false otherwise.
+	 */
+	public boolean isTraced(Class<? extends SimulationAction> actionClass) {
+		if (!traceEnabled)
+			return false;
+		
+		Boolean trace = actionTraces.get(actionClass);
+		if (trace == null)
+			return false;
+		else
+			return trace;
+	}
+	
+	
+}
