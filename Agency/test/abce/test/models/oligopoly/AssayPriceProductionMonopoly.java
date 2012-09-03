28a61c92dfa3a6bd898cb2ed39235c4e124c35aa
Matthew Rupp <ruppmatt@gmail.com>
2012-09-03 12:55:03 -0400

    Add endowment and prod cost to monopoly assay.
    Some of the initial work on the AssayPriceProductionMonpoly class were
    lost during the last merge.  I'm putting the code back in, and adding
    the expansion of the assay to include endowment and production costs. 
    Yes, some of the assay combinations are nonsensical (e.g. prod cost >
    price), but I want a full assay to test the model's dynamics.

--- Agency/test/abce/test/models/oligopoly/AssayPriceProductionMonopoly.java
+++ Agency/test/abce/test/models/oligopoly/AssayPriceProductionMonopoly.java
@@ -16,6 +16,9 @@ public class AssayPriceProductionMonopoly {
 	public static void main(String[] args){
 		
 
+		double[] endowments = {1000,10000,100000,1000000};
+		double[] prodcosts = {10,20,30,40,50,60,70,80,90,100,120};
+		
 		int[] steps = {1, 2, 8, 64};
 		
 		ArrayList<Double> prices = new ArrayList<Double>();
@@ -36,32 +39,46 @@ public class AssayPriceProductionMonopoly {
 			System.exit(1);
 		}
 		
-		for (int step: steps){
-			for (double price : prices){
-				for (double prod : production){
-					System.err.println("step:" + step + "  price:" + price + "  prod:" + prod);
-					OligopolySimulation sim = new OligopolySimulation(0, CONFIG_PATH, -1);
-					sim.getConfig().steps_to_run = step;
-					sim.initialize();
-					FixedProductionPricingFirm firm = new FixedProductionPricingFirm(price, prod);
-					sim.setupFirm(firm);
-					try {
-						sim.call();
-					} catch (Exception e) {
-						e.printStackTrace();
-						System.exit(1);
+		for (double endowment : endowments){
+			for (double prodcost: prodcosts){
+				for (int step: steps){
+					for (double price : prices){
+						for (double prod : production){
+							System.err.println("step:" + step + "  price:" + price + "  prod:" + prod);
+							OligopolySimulation sim = new OligopolySimulation(0, CONFIG_PATH, -1);
+							
+							/**
+							 * Configuration has to be set via the "setProperty()" method; the initialize
+							 * method at the end of block translates these properties into the the indiv
+							 * class members.
+							 */
+							sim.getConfig().setProperty("firm_endowment", Double.toString(endowment));
+							sim.getConfig().setProperty("cost_constant", Double.toString(prodcost));
+							sim.getConfig().setProperty("steps_to_run", Integer.toString(step));
+							sim.getConfig().setProperty("firm_initial_price", Double.toString(price));
+							sim.getConfig().setProperty("firm_initial_production", Double.toString(prod));
+							sim.initialize();
+							FixedProductionPricingFirm firm = new FixedProductionPricingFirm(price, prod);
+							sim.setupFirm(firm);
+							try {
+								sim.call();
+							} catch (Exception e) {
+								e.printStackTrace();
+								System.exit(1);
+							}
+							for (Firm f : sim.getFirms()){
+								fot.write(
+										sim.simulationID,
+										f.agentID,
+										step,
+										price,
+										prod,
+										f.getAccounts().getNetWorth()
+										);
+							}
+							
+						}
 					}
-					for (Firm f : sim.getFirms()){
-						fot.write(
-								sim.simulationID,
-								f.agentID,
-								step,
-								price,
-								prod,
-								f.getAccounts().getNetWorth()
-								);
-					}
-					
 				}
 			}
 		}
