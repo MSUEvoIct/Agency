08c4565f288a0e634158b10f86e6e7e6a843b44b
kkoning <kkoning@gmail.com>
2012-10-30 11:56:03 -0400

    Slight improvements to SpitefulCooperator; some aspects of behavior are now parameterized by the genome.

--- Agency/models/abce/models/io/iterated/cournot/investment/SpitefulCooperator.java
+++ Agency/models/abce/models/io/iterated/cournot/investment/SpitefulCooperator.java
@@ -2,28 +2,45 @@ package abce.models.io.iterated.cournot.investment;
 
 import ec.vector.FloatVectorIndividual;
 
+/**
+ * SpitefulCooperator
+ * 
+ * If the quantity in the last period was within a certain threshold of the monopoly
+ * quantity, SpitefulCooperator produces the cartel level of output.  If not, it
+ * reverts to a static quantity specified in its genome.  The values of the genome
+ * are used as follows:
+ * 
+ * genome[0]:  The default quantity if not cooperating as a cartel.
+ * genome[1]:  The amount of capital to purchase each step.
+ * ...[2]:  The accepted deviation from the cartel quantity for cooperation
+ * 
+ * @author kkoning
+ *
+ */
 public class SpitefulCooperator extends FloatVectorIndividual implements InvestmentFirm {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public double purchaseCapital(InvestmentModel sim, int marketNum) {
-		return 10.0;
+		return genome[1];
 	}
 
 	@Override
 	public double setProduction(InvestmentModel sim, int marketNum) {
 		double monopolyQty = sim.getMonopolyQty();
+		int numFirms = sim.qtyProduced.keySet().size();
+		
 		if (sim.step == 0)
-			return monopolyQty/3;
+			return monopolyQty / numFirms;
 		
 		double qtyLast = sim.totalQty(marketNum, sim.step-1);
 		
 		double deviation = Math.abs(qtyLast - monopolyQty);
 		
-		if (deviation < 5) {
-			return monopolyQty / 3;
+		if (deviation < genome[2]) {
+			return monopolyQty / numFirms;
 		} else {
-			return monopolyQty / 1.5;
+			return genome[0];
 		}
 	}
 
