beebf2dd8b80817526b3502bfff929f1f0325f1f
kkoning <kkoning@gmail.com>
2013-02-26 15:37:02 -0500

    Production stimulus now has two steps of past data

--- Agency/models/ec/agency/iteratedcournot/ProductionStimulus.java
+++ Agency/models/ec/agency/iteratedcournot/ProductionStimulus.java
@@ -2,8 +2,11 @@ package ec.agency.iteratedcournot;
 
 public class ProductionStimulus {
 	public float myLastProduction;
+	public float myLastProduction2;
 	public float othersLastProduction;
+	public float othersLastProduction2;
 	public float price;
+	public float price2;
 	public int step;
 
 }
