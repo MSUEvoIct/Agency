4e8158588f2cb9ee0b83d3e37b5055bd37265ff3
kkoning <kkoning@gmail.com>
2013-05-25 13:57:41 -0400

    Deleted more obsolete and unused test model code.

--- Agency/models/abce/io/ecj/PricingProblem.java
+++
@@ -1,7 +0,0 @@
-package abce.io.ecj;
-
-import ec.Problem;
-
-public class PricingProblem extends Problem {
-
-}
--- Agency/models/abce/io/ecj/ProductionProblem.java
+++
@@ -1,7 +0,0 @@
-package abce.io.ecj;
-
-import ec.Problem;
-
-public class ProductionProblem extends Problem {
-
-}
