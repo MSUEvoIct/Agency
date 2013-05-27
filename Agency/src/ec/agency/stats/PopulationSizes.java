13a87e913c31e39e4d8b6becd4209486a365a999
kkoning <kkoning@gmail.com>
2013-05-27 12:27:56 -0400

    Added class names of individuals per subpopulation into output headers, for reference in data.

--- Agency/src/ec/agency/stats/PopulationSizes.java
+++ Agency/src/ec/agency/stats/PopulationSizes.java
@@ -10,24 +10,24 @@ public class PopulationSizes extends Statistics {
 
 	static final String outFile = "popSizes";
 	String filename;
-	
+
 	boolean fileOpened = false;
 	boolean headerOutput = false;
 
 	DataOutputFile out;
-	
+
 	@Override
 	public void setup(EvolutionState evoState, Parameter base) {
 		super.setup(evoState, base);
-		
+
 		String fileName;
 		if (evoState.job != null)
 			fileName = outFile + ".job" + evoState.job[0];
 		else
 			fileName = outFile;
-		
+
 		out = new DataOutputFile(fileName);
-		
+
 	}
 
 	public void finalStatistics(EvolutionState state, int result) {
@@ -41,7 +41,7 @@ public class PopulationSizes extends Statistics {
 	@Override
 	public void postEvaluationStatistics(EvolutionState state) {
 		super.postBreedingStatistics(state);
-		
+
 		int numSubpops = state.population.subpops.length;
 		int size = numSubpops + 2;
 		if (!headerOutput) {
@@ -49,26 +49,27 @@ public class PopulationSizes extends Statistics {
 			headers[0] = "Job";
 			headers[1] = "Generation";
 			for (int i = 0; i < numSubpops; i++) {
-				headers[i+2] = "SubPop" + i;
+				String indName = state.population.subpops[i].species.i_prototype
+						.getClass().getSimpleName();
+				headers[i + 2] = "SubPop" + i + "_" + indName;
+
 			}
 			out.writeTuple(headers);
 			headerOutput = true;
 		}
-		
-		
-		
+
 		Object[] o = new Object[size];
-		if (state.job != null) 
+		if (state.job != null)
 			o[0] = state.job[0];
 		else
 			o[0] = null;
 		o[1] = state.generation;
-		
+
 		for (int i = 0; i < numSubpops; i++) {
-			o[i+2] = state.population.subpops[i].individuals.length;
+			o[i + 2] = state.population.subpops[i].individuals.length;
 		}
 		out.writeTuple(o);
 		out.flush();
 	}
-	
+
 }
