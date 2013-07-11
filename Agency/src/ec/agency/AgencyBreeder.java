2d9e0e5ef64d26ea5823ae49a43e817548626751
kkoning <kkoning@gmail.com>
2013-07-10 20:43:28 -0400

    Fixed problem with "subpopulation group" size calculation.

--- Agency/src/ec/agency/AgencyBreeder.java
+++ Agency/src/ec/agency/AgencyBreeder.java
@@ -1,4 +1,4 @@
- package ec.agency;
+package ec.agency;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -27,9 +27,13 @@ public class AgencyBreeder extends SimpleBreeder {
 	private static final String P_numSubpopGroups = "numSubpopGroups";
 	private static final String P_changeWeight = "changeWeight";
 	private static final String P_size = "size";
-	
+
 	int numSubpopGroups;
 	int tournamentSize;
+	/**
+	 * The # of individuals in each subpopulation group; as specified in the
+	 * configuration file.
+	 */
 	int[] groupSizes;
 	float changeWeight;
 
@@ -37,36 +41,39 @@ public class AgencyBreeder extends SimpleBreeder {
 	public void setup(EvolutionState state, Parameter base) {
 		super.setup(state, base);
 
-		tournamentSize = state.parameters.getInt(base.push(P_tournamentSize), null);
-		numSubpopGroups =  state.parameters.getInt(base.push(P_numSubpopGroups), null);
+		tournamentSize = state.parameters.getInt(base.push(P_tournamentSize),
+				null);
+		numSubpopGroups = state.parameters.getInt(base.push(P_numSubpopGroups),
+				null);
 		groupSizes = new int[numSubpopGroups];
-		
+
 		for (int i = 0; i < numSubpopGroups; i++) {
-			groupSizes[i] = state.parameters.getInt(base.push(P_numSubpopGroups).push("" + i).push(P_size), null);
+			groupSizes[i] = state.parameters.getInt(base
+					.push(P_numSubpopGroups).push("" + i).push(P_size), null);
 		}
-		
-		changeWeight = state.parameters.getFloat(base.push(P_changeWeight), null);
+
+		changeWeight = state.parameters.getFloat(base.push(P_changeWeight),
+				null);
 		if (changeWeight < 0)
 			throw new RuntimeException("breed.changeWeight must be in (0-1].");
 		if (changeWeight >= 1)
 			throw new RuntimeException("breed.changeWeight must be in (0-1].");
-			
+
 	}
-	
+
 	@Override
 	public Population breedPopulation(EvolutionState state) {
 		// How large are the new subpopulations going to be?
 		int[] subpopSizes = newSubpopSizes(state);
 
-		// 
-		for( int i = 0; i < subpopSizes.length; i++) {
+		//
+		for (int i = 0; i < subpopSizes.length; i++) {
 			Subpopulation s = state.population.subpops[i];
 			if (s instanceof ScalableSubpopulation) {
-				ScalableSubpopulation ss = (ScalableSubpopulation)s;
+				ScalableSubpopulation ss = (ScalableSubpopulation) s;
 				ss.setTargetSize(subpopSizes[i]);
 			}
 		}
-		
 
 		return super.breedPopulation(state);
 	}
@@ -78,16 +85,15 @@ public class AgencyBreeder extends SimpleBreeder {
 	 */
 	protected int[] newSubpopSizes(EvolutionState state) {
 		int numSubpops = state.population.subpops.length;
-		
+
 		int[] existingSize = new int[numSubpops];
 		int[] targetSize = new int[numSubpops];
-		
-		
+
 		// Populate existing sizes
 		for (int i = 0; i < numSubpops; i++) {
 			existingSize[i] = state.population.subpops[i].individuals.length;
 		}
-		
+
 		// Populate target sizes
 		// allocate minimums first, subtract sub of minimums from total
 		int[] remainingInds = groupSizes.clone();
@@ -95,21 +101,25 @@ public class AgencyBreeder extends SimpleBreeder {
 			for (int j = 0; j < numSubpops; j++) { // j = subpop
 				Subpopulation s = state.population.subpops[j];
 				if (s instanceof ScalableSubpopulation) {
-					int minSize = ((ScalableSubpopulation) s).getMinSize();
-					targetSize[j] += minSize;
-					remainingInds[i] -= minSize;
+					ScalableSubpopulation ss = (ScalableSubpopulation) s;
+					int thisSpopGroup = ss.getSubpopulationGroup();
+					if (thisSpopGroup == i) {
+						targetSize[j] += ss.getMinSize();
+						remainingInds[i] -= ss.getMinSize();
+					}
 				}
 			}
 		}
-		
+
 		// allocate remaining slots with tournament selection
 		// TODO: Make this modular?
 		for (int i = 0; i < numSubpopGroups; i++) { // i = subpop group
 			List<SimpleInd> collapsedInds = collectInds(state, i);
-			
+
 			while (remainingInds[i] > 0) {
 				List<SimpleInd> tournamentMembers = new ArrayList<SimpleInd>();
-				for (int j = 0; j < tournamentSize; j++) { // j = slot in tournament
+				for (int j = 0; j < tournamentSize; j++) { // j = slot in
+															// tournament
 					int numInds = collapsedInds.size();
 					int randIndex = state.random[0].nextInt(numInds);
 					tournamentMembers.add(collapsedInds.get(randIndex));
@@ -119,26 +129,26 @@ public class AgencyBreeder extends SimpleBreeder {
 				targetSize[luckySubpop]++;
 				remainingInds[i]--;
 			}
-			
+
 		}
-		
+
 		// Average the existing and targetsizes
 		int[] finalSizes = new int[numSubpops];
 		float stayWeight = 1 - changeWeight;
-		for (int i = 0; i< numSubpops; i++) {
-			finalSizes[i] = (int) (existingSize[i]*stayWeight + targetSize[i]*changeWeight);
+		for (int i = 0; i < numSubpops; i++) {
+			finalSizes[i] = (int) (existingSize[i] * stayWeight + targetSize[i]
+					* changeWeight);
 		}
-		
+
 		return finalSizes;
 
 	}
 
-	
 	private class SimpleInd implements Comparable<SimpleInd> {
 
 		int subPop;
 		float fitness;
-		
+
 		@Override
 		public int compareTo(SimpleInd o) {
 			if (fitness > o.fitness)
@@ -147,7 +157,7 @@ public class AgencyBreeder extends SimpleBreeder {
 				return -1;
 			return 0;
 		}
-		
+
 	}
 
 	private List<SimpleInd> collectInds(EvolutionState state, int subpopGroup) {
@@ -171,10 +181,7 @@ public class AgencyBreeder extends SimpleBreeder {
 			}
 		}
 		return toReturn;
-		
+
 	}
-	
-	
-	
-	
+
 }
